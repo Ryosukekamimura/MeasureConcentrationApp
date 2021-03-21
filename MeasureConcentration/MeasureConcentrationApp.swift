60abb4c25c1a24a37bf3a2c933ba3c85ef9db083
Ryosukekamimura <come1234560@gmail.com>
2021-03-21 19:48:31 +0900

    Initial Commit

---
+++ MeasureConcentration/MeasureConcentrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeasureConcentrationApp.swift
+//  MeasureConcentration
+//
+//  Created by 神村亮佑 on 2021/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MeasureConcentrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
